11578b6bcac5178d0e889924d89d06f267abfd0c
hengshuangliu <767924520@qq.com>
2017-09-11 20:50:28 +0800

    complete the document and REAMME.md

--- include/BadmintonField.h
+++ include/BadmintonField.h
@@ -9,21 +9,24 @@ class BadmintonField
 	public:
 		BadmintonField();
 		~BadmintonField();
-		
+		// set basic variables for the badmintonfield.
 		void setBadmintonField(char num, std::vector<int> weekF, std::vector<int> workF, double workC, double weekC);
 		void getBadmintonField(char& num, std::vector<int>& weekF, std::vector<int>& workF, double& workC, double& weekC);
+		// take a order or cancel in the badminton field.
 		bool takeOrder(std::string user, int y, int m, int d, int s, int e);
 		bool cancel(std::string user, int y, int m, int d, int s, int e);
+		// get the cancel fee or order fee.
 		double getCancelFee();
 		double getOrderFee();
 		char getFieldNum();
-		double printRecords(std::ostream& oser);
 		
 		double orderFee(int y, int m, int d, int s, int e); // calculate orderFee;
 		double cancelFee(int y, int m, int d, int s, int e); // calculate cancelFee;
 		std::vector<OrderRecord>::iterator findOrderRecord(int y, int m, int d, int s, int e);
 		std::vector<record>::iterator findRecord(int y, int m, int d, int s, int e);
-		// print single element in the records vector.
+		
+		// print element in the records vector.
+		double printRecords(std::ostream& oser);
 		void printRecord(std::ostream& oser, double& sum, std::vector<record>::iterator& it);
 		void printOrderRecord(std::ostream& oser, double& sum, std::vector<OrderRecord>::iterator& it_order);
 		
--- include/record.h
+++ include/record.h
@@ -6,21 +6,28 @@ class record
 {
 	public:
 		record();
+		
+		// y: year, m: month, d:day, s: started time, e: end time, charge_p: charge for this record.
 		record(int y, int m, int d, int s, int e, double charge_p);
 		~record();
 		
+		// Set date, times, charge for the record.
 		bool setDate(int y, int m, int d);
 		bool setTimes(int s, int e);
+		double setCharge(double total);
+		// get method.
 		void getDate(int& y, int& m, int& d);
 		void getTimes(int& start, int & end);
+		double getCharge();
+		// Validate the date and time interval.
 		bool dateIsValid(int y, int m, int d);
 		bool timeIsValid(int s, int e);
-		double getCharge();
-		double setCharge(double total);
+		
+		// overload operator for sorting algorithm.
 		bool operator<(record& b);
 		bool operator==(record& b);
 	protected:
-		int year;
+		int year;  
 		int month;
 		int day;
 		int time_s;
@@ -28,11 +35,14 @@ class record
 		double charge;
 };
 
+// Booking record should record the user's ID.
+// This OrderRecord is inherited from record.
 class OrderRecord : public record
 {
 	public:
 		OrderRecord(std::string user, int y, int m, int d, int s, int e, double charge_p);
 		~OrderRecord();
+		// set and get method.
 		bool setUserID(std::string user);
 		std::string getUserID();
 	private:
